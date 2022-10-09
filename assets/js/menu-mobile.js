b8324801a08a11e35e2283e59c46cfcbcc05bbee
Raphael Castro <113002682+raphacast@users.noreply.github.com>
2022-10-09 00:37:44 -0300

    Portifólio

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -11,3 +11,59 @@ menu.addEventListener('click', function () {
         document.getElementById('menumobile').style.display = 'none'
     }
 })
+
+
+// Scroll Menu 
+
+let homemob = document.querySelector('.homemob')
+homemob.addEventListener('click', homescrol)
+
+function homescrol() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+let sobremob = document.querySelector('.sobremob')
+sobremob.addEventListener('click', sobrescrol)
+
+function sobrescrol() {
+    window.scrollTo({
+        top: 609,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+let habmob = document.querySelector('.habmob')
+habmob.addEventListener('click', habscrol)
+
+function habscrol() {
+    window.scrollTo({
+        top: 1408,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+let pjmob = document.querySelector('.pjmob')
+pjmob.addEventListener('click', pjscrol)
+
+function pjscrol() {
+    window.scrollTo({
+        top: 2011,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+let contmob = document.querySelector('.contatomob')
+contmob.addEventListener('click', contscrol)
+
+function contscrol() {
+    window.scrollTo({
+        top: 2577,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
