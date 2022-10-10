45c06b73cbdf73c56c8e34daf2431ad502ba965d
Raphael Castro <113002682+raphacast@users.noreply.github.com>
2022-10-10 12:17:56 -0300

    Portifólio

--- assets/js/projeto.js
+++ assets/js/projeto.js
@@ -5,12 +5,13 @@ ver.addEventListener('click', projeto)
 function projeto(){
     if (document.querySelector(".section-projetos--pj").style.height == '320px') {
         document.querySelector(".section-projetos--pj").style.height = '700px';
-        document.querySelector(".section-conhecimento").style.height = '400px';
+        document.querySelector(".section-projetos-body h2").style.marginTop = '300px';
+        document.querySelector("footer").style.marginTop = '300px';
        
     } else {
         document.querySelector(".section-projetos--pj").style.height = '320px';
-        document.querySelector(".section-conhecimento").style.height = '400px';
-
+        document.querySelector(".section-projetos-body h2").style.marginTop = '0px';
+        document.querySelector("footer").style.marginTop = '0px';
        
 
     }
